09b9ecd785d623ccb9f32e212504616ebf89b06c
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-08-05 17:56:15 +0100

    require all needed stuff

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const stripe = require('stripe')(process.env.STR_PV)
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
+
 const app = express()
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log("Server listennig on port: " + port)
